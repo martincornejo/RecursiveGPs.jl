d9bb6a8f0a0304520f0e241b991e2f40f1ed7a43
JuanVictorSandalinas <go69lif@mytum.de>
2025-03-05 13:33:11 +0100

    Updated RGP to support different batch sizes
    Updated RGP implementation to support different batch sizes. Added additional test functions. Replaced data structures, switching from ComponentArray to dict for better performance/compatibility

--- rgp.jl
+++ rgp.jl
@@ -8,10 +8,6 @@ function data_preprocessing(X_basis, X_test, Y_test)
     X_test = X_test[shuffled_index, :]
     Y_test = Y_test[shuffled_index, :]
 
-    ## Stheno Notation
-    X_basis = RowVecs(X_basis)
-    X_test = RowVecs(X_test)
-    Y_test = RowVecs(Y_test)
     return X_basis, X_test, Y_test
 end
 
@@ -23,82 +19,70 @@ function gp_rgp0(X_basis, batch_size, θ)
         - 'X_basis' = Matrix (N_basis, D) of basis vectors
         - 'θ' = ComponentArray GP hyperparameters
     # Returns:
-        - 'p' = ComponentArray RGP Parameters
+        - 'p' = ComponentArray RGP cte Parameters
         - 'prior' = ComponentArray of Prior distributions
         - 'posterior' = ComponentArray of Posterior distributions
     """
 
-    k = with_lengthscale(SEKernel(), θ.l)
+    kernel = with_lengthscale(SEKernel(), θ.l)
 
     N_basis = size(X_basis, 1)
-    N_test = batch_size
 
     mu_basis = fill(θ.mu, N_basis)
-    mu_test = fill(θ.mu, N_test)
-    cov_basis = kernelmatrix(k, X_basis, X_basis)
+    mu_test = fill(θ.mu, batch_size)
+    cov_basis = kernelmatrix(kernel, X_basis, X_basis)
 
-    cov_test = zeros(N_test, N_test)
-    H = zeros(N_test, N_basis)
     inv_cov_basis = inv(cov_basis)
+    cov_test = zeros(batch_size, batch_size)
 
     ## Prior of p(g), p(gt). Covariance matrix  p(gt) defined at inference
-    prior = ComponentArray((;
-        basis=(;
-            mu=mu_basis,
-            cov=cov_basis,
-        ),
-        test=(;
-            mu=mu_test,
-            cov=cov_test,
-        ),
-    ))
+    # Set as Dict since test matrix can have different size at runtime
 
-    ## Posterior over p(g|y1:t) and  p(gt|y1:t) changes as new data arrives 
-    posterior = ComponentArray((;
-        basis=(;
-            mu=mu_basis,
-            cov=cov_basis
-        ),
-        test=(;
-            mu=mu_test,
-            cov=cov_test
-        ),
-    ))
+    prior = Dict(
+        :basis => Dict(:mu => mu_basis, :cov => cov_basis),
+        :test => Dict(:mu => mu_test, :cov => cov_test)
+    )
 
-    ## Parameters which are not hypertuned. H Initializes as nothing since depend X_test
+    ## Posterior over p(g|y1:t) and  p(gt|y1:t) changes as new data arrives
+    # Set as Dict since test matrix can have different size at runtime
+    posterior = Dict(
+        :basis => Dict(:mu => mu_basis, :cov => cov_basis),
+        :test => Dict(:mu => mu_test, :cov => cov_test)
+    )
+
+    ## Parameters which are not hypertuned and remain constant
     p = ComponentArray((;
-        k=k,
+        kernel=kernel,
         inv_cov_basis=inv_cov_basis,
-        H=H,
-        Q=θ.std^2 * I(N_test)
     ))
     return p, prior, posterior
 end
 
 
-function inference(p, prior, posterior, x_test, X_basis)
-    # PARALELIZABLE Matrix computation of kernelmatrix computations
+function inference(p, prior, posterior, X_batch, X_basis)
+    ## All kernelmatrix multiplication can be recomputed or paralellizable
     """
     Performs predict step of the posterior
     # Arguments:
        - 'p' = ComponentArray RGP Parameters
        - 'prior' = ComponentArray of Prior distributions
        - 'posterior' = ComponentArray of Posterior distributions
-       - 'x_test' = Matrix (N_test, D) of test points
+       - 'X_batch' = Matrix (N_batch, D) of batch test points
        - 'X_basis' = Matrix (N_basis, D) of basis vectors
     
     # Returns_
-        - 'posterior' = Predicted posterior of p(gt|y1:t-1)
+        - 'posterior' = Predicted posterior of p(g|y1:t-1)
     """
-    p.H = kernelmatrix(p.k, x_test, X_basis) * p.inv_cov_basis # eq.8 
-    posterior.test.mu = prior.test.mu + p.H * (posterior.basis.mu - prior.basis.mu) #eq.6 
-    R = kernelmatrix(p.k, x_test) - p.H * kernelmatrix(p.k, X_basis, x_test) #eq.7 
-    posterior.test.cov = R + p.H * posterior.basis.cov * p.H' #eq.9
-    return posterior
+    H = kernelmatrix(p.kernel, X_batch, X_basis) * p.inv_cov_basis # eq.8
+    posterior[:test][:mu] = prior[:test][:mu] + H * (posterior[:basis][:mu] - prior[:basis][:mu]) #eq.6 
+
+    R = kernelmatrix(p.kernel, X_batch) - H * kernelmatrix(p.kernel, X_basis, X_batch) #eq.7 
+    posterior[:test][:cov] = R + H * posterior[:basis][:cov] * H' #eq.9
+    return posterior, H
 end
 
 
-function update(p, posterior, y_test)
+function update(θ, posterior, H, Y_batch)
     """
     Performs update step of the posterior
 
@@ -106,38 +90,72 @@ function update(p, posterior, y_test)
        - 'p' = ComponentArray RGP Parameters
        - 'prior' = ComponentArray of Prior distributions
        - 'posterior' = ComponentArray of Posterior distributions
-       - 'y_test' = Matrix (N_test, D) of test points outputs
+       - 'y_test' = Matrix (N_batch, D) of batch test points
     
-    # Returns_
+    # Returns:
         - 'posterior' = Updated posterior of p(g|y1:t)
+        - 'H' = Observe-state matrix
     """
-    Gk = posterior.basis.cov * p.H' * inv(posterior.test.cov + p.Q) #eq.12
-    posterior.basis.mu = posterior.basis.mu + Gk * (y_test - posterior.test.mu) #eq.10
+    Gk = posterior[:basis][:cov] * H' * inv(posterior[:test][:cov] + θ.std^2 * I(size(Y_batch, 1))) #eq.12
 
-    posterior.basis.cov = posterior.basis.cov - Gk * p.H * posterior.basis.cov #eq.11
+    posterior[:basis][:mu] = posterior[:basis][:mu] + Gk * (Y_batch - posterior[:test][:mu]) #eq.10
+
+    posterior[:basis][:cov] = posterior[:basis][:cov] - Gk * H * posterior[:basis][:cov] #eq.11
 
     return posterior
 end
 
+function learning_step(p, prior, posterior, X_basis, X_batch, Y_batch, θ)
+    """
+    Performs one update step
+    """
 
-function rgp(X_basis, X_test, Y_test, θ, batch_size)
+    posterior, H = inference(p, prior, posterior, X_batch, X_basis)
+    posterior = update(θ, posterior, H, Y_batch)
+
+    return posterior
+end
+
+function rgp(X_basis, X_test, Y_test, θ, batch_size, save_data=false)
     """
     Does rgp without hyperparameter tuning
     # Arguments:
         - 'X_basis' = Matrix (N_basis, D) of basis vectors
-        - 'X_test' = Matrix (n_batches * N_test, D) of test points
-        - 'Y_test' = Matrix (n_batches * N_test, D) of test points
+        - 'X_test' = Matrix (N_test, D) of test points
+        - 'Y_test' = Matrix (N_test, D) of test points
         - 'θ' = ComponentArray of GP hypertuned parameters
+        - 'batch_size' = Number of samples per update
+        - 'save_data' = Set to true if save posterior of all iterations
     # Returns:
         - 'posterior' = ComponentArray of p(g:t) GP
     """
 
     X_basis, X_test, Y_test = data_preprocessing(X_basis, X_test, Y_test)
     p, prior, posterior = gp_rgp0(X_basis, batch_size, θ)
+    N_test = size(X_test, 1)
+    batch_div = div(N_test, batch_size)
+    batch_rem = rem(N_test, batch_size)
 
-    for (x_test, y_test) in zip(X_test, Y_test)
-        inference(p, prior, posterior, x_test, X_basis)
-        update(p, posterior, y_test)
+    for batch_start in 1:batch_size:N_test-batch_rem
+
+        batch_end = batch_start + batch_size - 1
+
+        X_batch = X_test[batch_start:batch_end]
+        Y_batch = Y_test[batch_start:batch_end]
+
+        learning_step(p, prior, posterior, X_basis, X_batch, Y_batch, θ)
+    end
+
+    # Last iteration if batch_size non divisible by N_test
+    if batch_rem != 0
+        prior[:test][:mu] = fill(θ.mu, batch_rem)
+        batch_start = N_test - batch_rem + 1
+        batch_end = N_test
+
+        X_batch = X_test[batch_start:batch_end]
+        Y_batch = Y_test[batch_start:batch_end]
+
+        learning_step(p, prior, posterior, X_basis, X_batch, Y_batch, θ)
     end
 
     return p, posterior
@@ -145,20 +163,22 @@ end
 
 
 function predict(p, posterior, x_predict)
+    ## Paralelizable if needed
     """
     Does a prediction using a posterior at X_predict
+    Note: If N_predict high (Ej.: 3000-5000 points), do by batches
     # Arguments:
-        - 'posterior' = Posterior distribition of p(g|y_test)
+        - 'posterior' = Posterior distribition with p(g|y_test)
         - 'x_predict' = Value to predict
     # Returns:
-        - 'mu_predict' = ComponentArray of predicted mean and cov
+        - 'mu_predict' = Mean prediction
         - 'C_predict' = Covariance prediction
     """
-    x_predict = RowVecs(x_predict)
-    H = kernelmatrix(p.k, x_predict, X_basis) * p.inv_cov_basis
-    mu_predict = H * posterior.basis.mu
-    R = kernelmatrix(p.k, x_predict) - H * kernelmatrix(p.k, X_basis, x_predict)
-    C_predict = R + H * posterior.basis.cov * H'
+
+    H = kernelmatrix(p.kernel, x_predict, X_basis) * p.inv_cov_basis
+    mu_predict = H * posterior[:basis][:mu]
+    R = kernelmatrix(p.kernel, x_predict) - H * kernelmatrix(p.kernel, X_basis, x_predict)
+    C_predict = R + H * posterior[:basis][:cov] * H'
 
     return mu_predict, C_predict
 
