430039e1edc88b9bfe262b6929ce2921bc91654e
Martin Cornejo <martin.cornejo@tum.de>
2025-03-19 09:24:15 +0100

    Add battery syntehtic data script - Add dependencies - Create ECM model with MTK - Add OCV and current profile look-up-tables - Create simulation to generate synthetic data

---
+++ ecm-mtk.jl
@@ -0,0 +1,71 @@
+using OrdinaryDiffEq
+using ModelingToolkit
+using ModelingToolkit: t_nounits as t, D_nounits as D
+
+using DataFrames
+using CSV
+using DataInterpolations
+
+using LinearAlgebra
+
+using CairoMakie
+
+@mtkmodel ECM begin
+    @parameters begin
+        Q = 4.8
+        R0 = 15e-3
+        R1 = 15e-3
+        τ1 = 60.0
+        R2 = 15e-3
+        τ2 = 600.0
+    end
+    @structural_parameters begin
+        focv
+        fi
+    end
+    @variables begin
+        i(t), [input = true]
+        v(t), [output = true]
+        vr(t)
+        v1(t) = 0.0
+        v2(t) = 0.0
+        ocv(t)
+        soc(t)
+    end
+    @equations begin
+        D(soc) ~ i / (Q * 3600.0)
+        D(v1) ~ -v1 / τ1 + i * (R1 / τ1)
+        D(v2) ~ -v2 / τ2 + i * (R2 / τ2)
+        vr ~ i * R0
+        ocv ~ focv(soc)
+        i ~ fi(t)
+        v ~ ocv + vr + v1 + v2
+    end
+end
+
+begin # read OCV look-up-table and current profile
+    df_ocv = CSV.File("data/ocv.csv") |> DataFrame
+    focv = LinearInterpolation(df_ocv.ocv, df_ocv.soc, extrapolation=ExtrapolationType.Constant)
+
+    df = CSV.File("data/profile.csv") |> DataFrame
+    fi = ConstantInterpolation(df.i, df.t)
+end
+
+begin # create model
+    @mtkbuild ecm = ECM(; focv, fi)
+    tspan = (0, 24 * 3600) # one day
+    ode = ODEProblem{false}(ecm, [ecm.soc => 0.5], tspan, [])
+end
+
+begin # create synthetic data
+    Ts = 1.0 # time sampling
+    sol = solve(ode, Tsit5(); saveat=Ts)
+
+    v = sol[ecm.v]
+    s = sol[ecm.soc]
+
+    # plot
+    lines(sol.t / 3600, v; axis=(; xlabel="Time in h", ylabel="Voltage in V")) |> display
+    lines(sol.t / 3600, s; axis=(; xlabel="Time in h", ylabel="SOC in p.u.")) |> display
+end
+
